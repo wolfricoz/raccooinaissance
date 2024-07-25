aece609b1bd50267727a4a4d83ccf179e81afb74
wolfricoz <ricoisdenaam@gmail.com>
2024-07-25 23:28:35 +0200

    Added volume to the settings and updated the pause screen!

--- scripts/pause_sceen.cs
+++ scripts/pause_sceen.cs
@@ -4,9 +4,14 @@ namespace Raccoonnaissance.scenes;
 
 public partial class pause_sceen : CanvasLayer
 {
+	private HSlider _volumeSlider;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_volumeSlider = GetNode<HSlider>("settings/GridContainer/VolumeSlider");
+		_volumeSlider.Value = 0;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,5 +49,32 @@ public partial class pause_sceen : CanvasLayer
 		GameManager.Singleton.IsPaused = false;
 		GD.Print("Resume button pressed");
 		this.Hide();
+	}
+
+	private void _on_main_menu_pressed()
+	{
+		GameManager.Singleton.ChangeScene("res://start_screen.tscn");
+	}
+
+
+	private void _on_quit_pressed()
+	{
+		GetTree().Quit();
+	}
+
+
+	private void _on_h_slider_value_changed(float value)
+	{
+		if ((int)value == -40)
+		{
+			// turn off main bus
+			AudioServer.SetBusMute(AudioServer.GetBusIndex("Master"), true);
+			return;
+		}
+		// turn on main bus
+		AudioServer.SetBusMute(AudioServer.GetBusIndex("Master"), false);
+		// Change the volume, the value is in decibels and 0 is the default, any higher
+		// than 10 will HURT your ears.. ouch.
+		AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Master"), value);
 	}
 }
